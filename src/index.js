6737ad945bb6dcb0713315bbff897d8ef4f65076
azmims <13519151@std.stei.itb.ac.id>
2022-03-20 20:55:02 +0700

    setup spotify api key in .env

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// const API_KEY = process.env.SPOT IFY_API_KEY;
+const API_KEY = process.env.REACT_APP_SPOTIFY_API_KEY;
+const API = {API_KEY}
+console.log(API);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
