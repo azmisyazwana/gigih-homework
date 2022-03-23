57d6c615cb7fd0159529fca99063dd9ece5cda21
azmims <13519151@std.stei.itb.ac.id>
2022-03-23 16:53:16 +0700

    add card song

---
+++ src/component/Song.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Song(){
+    return(
+        <div>
+            <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" />
+            <p>Bohemian Rhapsody (The Original Soundtrack)</p>
+            <p>Queen</p>
+            <button>Select</button>
+        </div>
+    )
+}
+
+export default Song;
