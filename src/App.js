39c20e15cf30d9d398b9826bf85389ff4998eeb1
azmims <13519151@std.stei.itb.ac.id>
2022-03-25 14:50:46 +0700

    homework module2-session2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Song from './component/Song';
+import data from './Data/data';
 
 function App() {
   return (
     <div className="App">
-      <Song />
+      <Song img={data.album.images[1].url} title={data.album.name} artist={data.artists[0].name} />
     </div>
   );
 }
--- src/component/Song.js
+++ src/component/Song.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Song(){
+function Song(props){
     return(
         <div>
-            <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" />
-            <p>Bohemian Rhapsody (The Original Soundtrack)</p>
-            <p>Queen</p>
+            <img src={props.img} />
+            <p>{props.title}</p>
+            <p>{props.artist}</p>
             <button>Select</button>
         </div>
     )
