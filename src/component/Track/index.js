a91a73620221c53268eb81284409b9ec56001b7a
azmims <13519151@std.stei.itb.ac.id>
2022-03-28 17:11:08 +0700

    fix homework module 2-session 3 (loop)

--- src/component/Track/index.js
+++ src/component/Track/index.js
@@ -1,26 +1,17 @@
 import data2 from "../../Data/data2";
 import Song from "../Song"
-import React from "react";
 
-class Track extends React.Component{
-    render(){data2.map((e, index) => {
-        return (
-            <div>
-                <img src={e.album.images[1].url} alt="" />
-                <p>{e.album.name}</p>
-                <p>{e.artists[0].name}</p>
-            </div>
-        );
-    });
+let songList = data2.map((e, index) => {
+    return <Song key={index} img={e.album.images[1].url} title={e.album.name} artist={e.artists[0].name} />
+});
+
+function Track(){
+    return(
+        <div>
+            <h1>LOOP TRACK</h1>
+            {songList}
+        </div>
+    )
 }
-}
 
-// const loop = data2.map((e, index) => {
-//     <Song key={index} img={e.album.images[1].url} title={e.album.name} artist={e.artists[0].name} />
-//     console.log(e.album.images[1].url)
-//     console.log(e.album.name)
-//     console.log(e.artists[0].name)
-// })
-
-// console.log(loop)
 export default Track;
